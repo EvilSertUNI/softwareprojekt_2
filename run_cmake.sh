136c8a588559b7bd44ff5b6aca9d6a14f4d2d5a6
SimonStraub2005 <straub.simon-it24@it.dhbw-ravensburg.de>
2025-06-09 20:10:47 +0000

    To build the whole thing we use this file

--- run_cmake.sh
+++ run_cmake.sh
@@ -1,4 +1,4 @@
-cmake -G "Unix Makefiles" -S . -B build -D CMAKE_C_COMPILER=g++ -DCMAKE_BUILD_TYPE=Release
+cmake -G "Unix Makefiles" -S . -B build -D CMAKE_C_COMPILER=gcc -DCMAKE_BUILD_TYPE=Release
 cd build
 make
 
