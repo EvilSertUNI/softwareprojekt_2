b68b054bceaaebe7eced35da0427d3d9d177d0d5
SimonStraub2005 <straub.simon-it24@it.dhbw-ravensburg.de>
2025-06-09 20:09:31 +0000

    Adding the prototyp of the Developer File

--- include/developer.hpp
+++ include/developer.hpp
@@ -1,5 +1,26 @@
-#ifndef DEVELOPER_6R3TPVVW_HPP
-#define MYCLASS_6R3TPVVW_HPP
+#pragma once
+#include <string>
+#include <iostream>
 
+class Developer {
+protected:
+    std::string name;
+    std::string alias;
+    std::string logo;
 
-#endif
+public:
+    Developer(const std::string& name, const std::string& alias);
+    virtual ~Developer() = default;
+
+    static void drink_coffee();
+
+    void load_logo_from_file(const std::string& filename);
+
+    std::string get_name() const;
+    std::string get_alias() const;
+    std::string get_logo() const;
+
+    virtual void solve_problem() const = 0;
+
+    friend std::ostream& operator<<(std::ostream& os, const Developer& dev);
+};
