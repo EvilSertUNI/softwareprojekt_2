9a86173581f958f5cd6dccdc257f6edc845daca2
SimonStraub2005 <straub.simon-it24@it.dhbw-ravensburg.de>
2025-06-11 12:26:06 +0000

    adding the comments for our senior cpp file

--- src/senior_developer.cpp
+++ src/senior_developer.cpp
@@ -1,8 +1,10 @@
 #include "senior_developer.hpp"
 
+//pass parameters to the base class constuctor
 SeniorDeveloper::SeniorDeveloper(const std::string& name, const std::string& alias)
     : Developer(name, alias) {}
 
+// Implementation of problem solving for serniors
 void SeniorDeveloper::solve_problem() const {
     std::cout << "Solving a problem:\n" << *this;
     std::cout << "Puh, that was tough" << std::endl;
