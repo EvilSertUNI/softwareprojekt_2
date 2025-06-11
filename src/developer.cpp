49dfa47e971f0d663a922a7e1f89fcd0aeb1570c
SimonStraub2005 <straub.simon-it24@it.dhbw-ravensburg.de>
2025-06-11 12:24:33 +0000

    adding the comments for our developer header

--- src/developer.cpp
+++ src/developer.cpp
@@ -3,26 +3,31 @@
 #include <stdexcept>
 #include <iterator>
 
+// Initialize name and alias
 Developer::Developer(const std::string& name, const std::string& alias)
     : name(name), alias(alias), logo("") {}
 
+// Static method: Simulates a developer drinking coffee
 void Developer::drink_coffee() {
     std::cout << "Ahhhh, I needed that coffee!!!" << std::endl;
 }
 
+// Loads logo content from file into logo string
 void Developer::load_logo_from_file(const std::string& filename) {
     std::ifstream file(DEV_LOGO_PATH + filename);
     if (!file) {
         throw std::runtime_error("Logo file could not be opened: " + filename);
     }
     logo.assign((std::istreambuf_iterator<char>(file)),
-                 std::istreambuf_iterator<char>());
+                std::istreambuf_iterator<char>());
 }
 
+// Getter methods
 std::string Developer::get_name() const { return name; }
 std::string Developer::get_alias() const { return alias; }
 std::string Developer::get_logo() const { return logo; }
 
+// Stream output operator: Prints logo, name, and alias
 std::ostream& operator<<(std::ostream& os, const Developer& dev) {
     if (!dev.logo.empty()) {
         os << dev.logo << std::endl;
