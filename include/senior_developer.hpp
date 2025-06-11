5288b54be6edaa9123bd841dbcd7a0a078a8b096
SimonStraub2005 <straub.simon-it24@it.dhbw-ravensburg.de>
2025-06-11 12:30:17 +0000

    at least our senior header file gets comments too

--- include/senior_developer.hpp
+++ include/senior_developer.hpp
@@ -1,8 +1,11 @@
 #pragma once
 #include "developer.hpp"
 
+//derived class of the base class Developer to represent the senior developer
 class SeniorDeveloper : public Developer {
 public:
+    //The Constructor calls the base class constructor
     SeniorDeveloper(const std::string& name, const std::string& alias);
+    //Implements solving a problem in senior style
     void solve_problem() const override;
 };
