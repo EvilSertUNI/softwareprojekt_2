6ed0a4a5b877fbe3432de3af68bde87184acb23a
SimonStraub2005 <straub.simon-it24@it.dhbw-ravensburg.de>
2025-06-11 12:29:39 +0000

    we also equip our junior header file with comments

--- include/junior_developer.hpp
+++ include/junior_developer.hpp
@@ -1,8 +1,12 @@
 #pragma once
 #include "developer.hpp"
 
+//derived class of the base class Developer to represent the junior developer
 class JuniorDeveloper : public Developer {
 public:
+    //The Constructor calls the base class constructor
     JuniorDeveloper(const std::string& name, const std::string& alias);
+    
+    //Implements solving a problem in junior style
     void solve_problem() const override;
 };
