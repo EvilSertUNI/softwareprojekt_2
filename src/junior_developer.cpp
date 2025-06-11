79c0d2493ce9bdfbed238fa837e1180637fb6c52
SimonStraub2005 <straub.simon-it24@it.dhbw-ravensburg.de>
2025-06-11 12:25:33 +0000

    adding the comments for our junior cpp file

--- src/junior_developer.cpp
+++ src/junior_developer.cpp
@@ -1,8 +1,10 @@
 #include "junior_developer.hpp"
 
+//pass parameters to the base class constuctor
 JuniorDeveloper::JuniorDeveloper(const std::string& name, const std::string& alias)
     : Developer(name, alias) {}
 
+// Implementation of problem solving for juniors
 void JuniorDeveloper::solve_problem() const {
     std::cout << "Solving a problem:\n" << *this;
     std::cout << "Ha, that was EZ!" << std::endl;
